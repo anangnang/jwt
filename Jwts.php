f93fad0631e35dbe92709fc2e3a2f86bcc70d249
anangnang <anang005@gmail.com>
2019-02-05 10:31:49 +0700

    Update Jwts.php

--- Jwts.php
+++ Jwts.php
@@ -14,35 +14,69 @@ class Jwts extends REST_Controller {
         $this->load->model('M_main');
     }
 
+    // noperk
+    public function noperk_get($id){
+        $jwt = $this->input->get_request_header('Authorization');
+        try {
+            $decode = JWT::decode($jwt,$this->secretkey,array('HS256'));
+            if ($this->M_main->is_valid($decode-> noHP)>0) {
+                $nik = $this->M_main->detail_user_coba($id);
+                $noperk = $this->M_main->cek_noperk1($nik); 
+                $output['error'] = false;
+                $output['msg'] = "Data Ditemukan";
+                $output['nik'] = $nik;
+                $output['data'] = $noperk;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        } catch (Exception $e) {
+            $output['error'] = true;
+            $output['status'] = 200;
+            $output['response'] = 'Unauthorized';
+            $this->set_response($output, REST_Controller::HTTP_OK);
+            //exit();
+        }
+    }
+    // end
 
     public function login_post()
     {
         $u = $this->post('noHP');
         $p = sha1($this->post('password'));
         $q = array('noHP' => $u); 
-        $invalidLogin['error'] = true;
-        $invalidLogin['status'] = 200;
-        $invalidLogin['response'] = 'Invalid Login';
+       
         $val = $this->M_main->get_user($q)->row(); 
-        if($this->M_main->get_user($q)->num_rows() == 0){$this->response($invalidLogin, REST_Controller::HTTP_OK);}
-		$match = $val->password;  
+        if($this->M_main->get_user($q)->num_rows() == 0){
+            
+            $this->response([
+                "error"=>true,
+                "msg"=>"Login gagal",
+                "data"=>null
+            ], REST_Controller::HTTP_OK);
+        }
+        $match = $val->password;  
         if($p == $match){  
-        	//$token['id'] = $val->id;
+            //$token['id'] = $val->id;
             $token['noHP'] = $u;
             $date = new DateTime();
             $token['iat'] = $date->getTimestamp();
             $token['exp'] = $date->getTimestamp() + 60*60*5; 
-            $output['error'] = false;
-            $output['status'] = 200;
             $outputs['token'] = JWT::encode($token,$this->secretkey); 
             $outputs['id'] = $val->id;
             $this->M_main->login($outputs['token'],$outputs['id']); 
-            $output['data']=$this->M_main->logined($outputs['token'],$outputs['id']); 
-            $this->set_response($output, REST_Controller::HTTP_OK);
+     //       $output['data']=$this->M_main->logined($outputs['token'],$outputs['id']); 
+            
+            $data=$this->M_main->logined($outputs['token'],$outputs['id']); 
+            $this->set_response([
+                "error" => false,
+                "msg" => "Login sukses",
+                "data" => $data
+            ], REST_Controller::HTTP_OK);
         }
         else {
             $output['error'] = true;
             $output['status'] = 200;
+            $output['data'] = null;
+            $output['msg'] = "Login gagal";
             $this->set_response($output, REST_Controller::HTTP_OK);
         }
     }
@@ -62,6 +96,30 @@ class Jwts extends REST_Controller {
             $output['nik'] = $nik;
             $output['noHP'] = $noHP;
             $this->set_response($output, REST_Controller::HTTP_OK); 
+      
+    }
+
+
+    public function verifikasi_put($id)
+    {   
+        $cek_token=$this->cektoken_post();
+        if($cek_token){
+        $kode = $this->put('otp'); 
+        if($kode==1234){
+            $this->M_main->verifikasi_user($id); 
+            $output['error'] = false;
+            $output['message'] = 'User Verified';
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+        else{
+            $output['error'] = true;
+            $output['message'] = 'User Not Verified';
+            $this->set_response($output, REST_Controller::HTTP_OK); 
+        }
+        }
+        else{
+            exit('Wrong Token');
+        }
       
     }
 
@@ -128,27 +186,27 @@ class Jwts extends REST_Controller {
         }
     }
 
-	public function noperk_get($id){
-        $jwt = $this->input->get_request_header('Authorization');
-        try {
-            $decode = JWT::decode($jwt,$this->secretkey,array('HS256'));
-            if ($this->M_main->is_valid($decode-> noHP)>0) {
-                $nik = $this->M_main->detail_user_coba($id);
-                $noperk = $this->M_main->cek_noperk1($nik); 
-                $output['error'] = false;
-                $output['status'] = 200;
-                $output['nik'] = $nik;
-                $output['dataUmum'] = $noperk;
-                $this->set_response($output, REST_Controller::HTTP_OK);
-            }
-        } catch (Exception $e) {
+    public function ambil_antrian_get($perkara_id){
+        $cek_token=$this->cektoken_post();
+        if($cek_token){
+        $waktu = $this->post('waktu'); 
+        $pagi=123;
+        if($waktu==$pagi){
             $output['error'] = true;
-            $output['status'] = 200;
+            $output['status'] = 401;
             $output['response'] = 'Unauthorized';
             $this->set_response($output, REST_Controller::HTTP_OK);
-            //exit();
+        }
+        else{ 
+            $output['error'] = false;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+        }
+        else{
+            exit('Wrong Token');
         }
     }
+
 
     public function sidang_get($nik){
         $jwt = $this->input->get_request_header('Authorization');
@@ -163,6 +221,23 @@ class Jwts extends REST_Controller {
         } catch (Exception $e) {
             $output['error'] = true;
             $output['status'] = 401;
+            $output['response'] = 'Unauthorized';
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function sidang_perkara_get($perkara_id){
+        $jwt = $this->input->get_request_header('Authorization');
+        try {
+            $decode = JWT::decode($jwt,$this->secretkey,array('HS256'));
+            if ($this->M_main->is_valid($decode-> noHP)>0) {
+                $sidang = $this->M_main->get_jadwal_sidang_per_perkara($perkara_id);
+                 $this->response($sidang);
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        } catch (Exception $e) {
+            $output['error'] = true;
+            $output['status'] = 200;
             $output['response'] = 'Unauthorized';
             $this->set_response($output, REST_Controller::HTTP_OK);
         }
@@ -221,8 +296,6 @@ class Jwts extends REST_Controller {
         }
 
     }
-
-
 
 
 }
